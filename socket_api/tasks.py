7111ab3415676a0353c646cca992c32f7286ebf7
yenoh <tessemayenoh94@gmail.com>
2024-06-18 20:16:39 +0300

    Removed email conf

--- socket_api/tasks.py
+++ socket_api/tasks.py
@@ -4,10 +4,7 @@ from django.core.mail import send_mail
 from channels_tutorial.settings import EMAIL_HOST_USER
 @shared_task
 def send():
-    subject = 'welcome to GFG world'
-    message = f'Hi arsenal, thank you for registering in geeksforgeeks.'
-    email_from = EMAIL_HOST_USER
-    recipient_list = ["arsenalgunner636@gmail.com", "tessemayenoh94@gmail.com" ]
+
     print("sending....")
     # send_mail( subject, message, email_from, recipient_list )
     return 1
